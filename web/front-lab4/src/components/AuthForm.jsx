7ee142e55e0e1d4b3a4c121ae8041ccf79b91e78
Grigory Voronoff <408402@edu.itmo.ru>
2024-12-10 23:38:39 +0300

    auth form fixes

--- web/front-lab4/src/components/AuthForm.jsx
+++ web/front-lab4/src/components/AuthForm.jsx
@@ -2,32 +2,75 @@ import Typography from "@mui/material/Typography";
 import PropTypes from 'prop-types';
 import Grid from '@mui/material/Grid2';
 import Box from "@mui/material/Box";
+import { Paper, TextField} from "@mui/material";
+import {styled} from '@mui/material/styles';
+import {PasswordInput} from "./PasswordInput.jsx";
 
 AuthForm.propTypes = {
     isLogin: PropTypes.bool.isRequired,
 };
 
+const MyPaperBox = styled(Paper)(({ elevation, theme}) => ({
+    // backgroundColor: '#fff',
+    padding: theme.spacing(3),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+    elevation: elevation
+}));
+
 export function AuthForm(props) {
     const {isLogin} = props;
 
     return (
-        <>
-            <Typography variant="h3">
+        // внешний бокс, чтобы всё выровнять
+        <Box
+            display="flex"
+            flexDirection="column"
+            alignItems="center"
+            justifyContent="center"
+            sx={{p: 5}}
+
+        >
+            <Typography variant="h3" align="center" gutterBottom>
                 {isLogin ? "Вход" : "Регистрация"}
             </Typography>
-            <form>
-                <Box sx={{
-                    flexGrow: 0.7,
-                    border: '1px solid grey',
-                    borderRadius: 5,
-                    bgcolor : 'lightgrey',
-                    
-                }}>
-                    <Grid container spacing={2}>
-                        <Grid xs={12}>asdas</Grid>
+
+            <MyPaperBox sx={{width: {xs:'90%', sm:'70%', md:'25%'}}} elevation={4}>
+                <form>
+                    <Grid
+                        container
+                        columnSpacing={2}
+                        rowSpacing={2}
+                    >
+                        <Grid size={{xs: 12, md: 3}} item display="flex" justifyContent="center" alignItems="center">
+                            <Typography>Логин</Typography>
+                        </Grid>
+                        <Grid item size={{xs: 12, md: 9}}>
+                            <TextField
+                                id="outlined-required"
+                                label="Имя пользователя"
+                                fullWidth
+                            >
+                            </TextField>
+                        </Grid>
+                        <Grid size={{xs: 12, md: 3}} item display="flex" justifyContent="center" alignItems="center">
+                            <Typography>Пароль</Typography>
+                        </Grid>
+                        <Grid item size={{xs: 12, md: 9}}>
+                            <PasswordInput ></PasswordInput>
+                        </Grid>
+                        {isLogin ? (<>
+                            <Grid size={{xs: 12, md: 3}} item display="flex" justifyContent="center" alignItems="center">
+                                <Typography>Повтор</Typography>
+                            </Grid>
+                            <Grid item size={{xs: 12, md: 9}}>
+                                <PasswordInput ></PasswordInput>
+                            </Grid>
+                        </>)
+                        : null}
                     </Grid>
-                </Box>
-            </form>
-        </>
+                </form>
+            </MyPaperBox>
+        </Box>
     );
 }
---
+++ web/front-lab4/src/components/PasswordInput.jsx
@@ -0,0 +1,47 @@
+import Grid from "@mui/material/Grid2";
+import Typography from "@mui/material/Typography";
+import {FormControl, InputAdornment, InputLabel, OutlinedInput} from "@mui/material";
+import IconButton from "@mui/material/IconButton";
+import {Visibility, VisibilityOff} from "@mui/icons-material";
+import {useState} from "react";
+
+
+export function PasswordInput(){
+    const [showPassword, setShowPassword] = useState(false);
+
+    const handleClickShowPassword = () => setShowPassword((show) => !show);
+
+    const handleMouseDownPassword = (event) => {
+        event.preventDefault();
+    };
+
+    const handleMouseUpPassword = (event) => {
+        event.preventDefault();
+    };
+
+    return (<>
+            <FormControl variant="outlined" fullWidth>
+                <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
+                <OutlinedInput
+                    id="outlined-adornment-password"
+                    required={true}
+                    type={showPassword ? 'text' : 'password'}
+                    endAdornment={
+                        <InputAdornment position="end">
+                            <IconButton
+                                aria-label={
+                                    showPassword ? 'hide the password' : 'display the password'
+                                }
+                                onClick={handleClickShowPassword}
+                                onMouseDown={handleMouseDownPassword}
+                                onMouseUp={handleMouseUpPassword}
+                                edge="end"
+                            >
+                                {showPassword ? <VisibilityOff/> : <Visibility/>}
+                            </IconButton>
+                        </InputAdornment>
+                    }
+                    label="Password"
+                /></FormControl>
+    </>)
+}
